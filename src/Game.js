2cf3c7a7fa27c650ff667f687f64c31f3aa4b577
Niina Kettunen <71032872+Niinamaaria@users.noreply.github.com>
2020-12-16 19:12:35 +0200

    Add files via upload

---
+++ src/Game.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Stats from './Stats';
+import Booster from './Booster';
+import Burger from './Burger';
+
+class Game extends Component {
+
+constructor(props){
+    super(props);
+
+    this.mouseClicked = this.mouseClicked.bind(this);
+    }
+    
+    mouseClicked(){
+        const clicks = this.props.clicks + 1;
+        this.props.setClicks(clicks);
+
+    }
+
+  render() {
+    return (
+      <>
+         <div className="header">
+         <h1>Burger Clicker</h1>
+         </div>
+         <div className="content content--justified">
+         <Stats count={this.props.clicks} />
+         <Burger onClick={this.mouseClicked} />
+         <Booster burgers={1}/>
+        </div>
+        </>
+        );
+    }
+}
+
+export default Game;
